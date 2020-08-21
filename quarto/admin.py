dc0594b55e0e7f70f61aa0760e3a666a02e74be6
Jorge Salgado <jaso_98@hotmail.com>
2020-08-21 13:24:45 -0500

    <New app>: We modified models and api app

--- quarto/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User, Room, Images_Room, Favorites
-
-admin.site.register(User,UserAdmin)
-admin.site.register(Room)
-admin.site.register(Images_Room)
-admin.site.register(Favorites)
